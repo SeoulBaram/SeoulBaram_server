083974471dd8877ca9a1fc1c724363376c017204
SOGNA <win778500@gmail.com>
2018-08-07 02:50:34 +0900

    어떤 데이터 사용할 지 후보군 선정

---
+++ module/schedule/weather_RealTime.js
@@ -0,0 +1,28 @@
+/*
+==================================================================
+[날씨, 대기정보와 관련된 API를 이용하여 독자적인 데이터베이스 구축]
+
+******************************************************************
+사용할 API 목록
+
+
+******************************************************************
+==================================================================
+후보
+**서울데이터포털
+서울시 관측소별 실시간 기상관측 현황
+http://data.seoul.go.kr/dataList/datasetView.do?infId=OA-2225&srvType=S&serviceKind=1&currentPageNo=1
+
+서울시 실시간 대기환경 평균 현황
+http://data.seoul.go.kr/dataList/datasetView.do?infId=OA-1201&srvType=S&serviceKind=1&currentPageNo=1
+
+서울시 실시간 자치구별 대기환경 현황
+http://data.seoul.go.kr/dataList/datasetView.do?infId=OA-1200&srvType=S&serviceKind=1&currentPageNo=1
+
+**공공데이터포털
+동네예보정보조회서비스
+https://www.data.go.kr/dataset/15000099/openapi.do
+
+한국환경공단_대기오염정보 조회 서비스
+https://www.data.go.kr/dataset/15000581/openapi.do
+ */
